6873869aa1c6220482a01323fdd1d45e100f89d2
Zachary Dowell <zdowell97@gmail.com>
2022-07-21 16:37:11 -0400

    added home page view

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', function(req,res) {
-    res.send("hello world")
+    res.render("home")
 })
 
 app.get('*', (req,res)=>{
     res.status(404).send('<h1>404 Page</h1>')
 })
-app.listen(process.en.PORT)
+app.listen(process.env.PORT)
---
+++ views/home.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const def = require('./default')
+
+function home () {
+    return (
+        <def>
+            <main>
+                <h1>HOME</h1>
+            </main>
+        </def>
+    )
+}
+
+module.exports = home
