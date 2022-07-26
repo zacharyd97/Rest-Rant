ff94a84a460264e1745dcfa2dbed305d671dd104
Zachary Dowell <zdowell97@gmail.com>
2022-07-26 18:47:50 -0400

    bug

--- controllers/places.js
+++ controllers/places.js
@@ -9,6 +9,20 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    console.log({place:places[id]})
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
   if (isNaN(id)) {
