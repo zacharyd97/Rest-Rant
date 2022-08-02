55aed6b786ffb3e046961fe770e8ad13766607ba
Zachary Dowell <zdowell97@gmail.com>
2022-08-02 17:42:24 -0400

    finished

--- models/places.js
+++ models/places.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: {type: String, required: true},
-  pic: String,
+  pic: {type: String, default: 'http://placekitten.com/350/350'},
   cuisines: {type: String, required: true},
   city: {type: String, default: 'Anytown'},
   state: {type: String, default: "USA"},
-  founded: Number
+  founded: {
+    type: Number,
+    min: [1673, 'Surely not taht old!'],
+    max: [new Date().getFullYear() , 'hey, this year is in the future']
+  }
 })
 
 placeSchema.methods.showEstablished = function() {
