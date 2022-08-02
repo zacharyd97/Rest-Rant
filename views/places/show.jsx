ab0685c5400851c0419cef10f85d809771284e09
Zachary Dowell <zdowell97@gmail.com>
2022-08-02 17:27:42 -0400

    finished method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,12 +5,22 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
-            <p>{data.place.cuisines}</p>
+            <div className='row'>
+            <div className='col-sm-6'>
             <img src={data.place.pic} alt="pic of place"/>
-            <p>{data.place.city}</p>
-            <p>{data.place.state}</p>
-            <p>{data.place.founded}</p>
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+            </div>
+            <div className='col-sm-6'>
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <h2>Not Rated</h2>
+            <h2>Description</h2>
+            <h4>
+            {data.place.showEstablished()}
+              serving {data.place.cuisines}
+            </h4>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>     
@@ -18,7 +28,9 @@ function show (data) {
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
-            </form>     
+            </form>
+            </div>
+            </div>     
           </main>
         </Def>
     )
